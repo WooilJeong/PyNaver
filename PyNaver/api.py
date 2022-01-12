bbfba6595da433ab913ec35f4dbc557f9eb0d059
WooilJeong <wooil@kakao.com>
2022-01-12 22:47:29 +0900

    init

---
+++ PyNaver/api.py
@@ -0,0 +1,80 @@
+import json
+import logging
+import pandas as pd
+import urllib.request
+
+
+## 네이버 데이터랩 API
+class Datalab:
+    """
+    네이버 데이터랩 API 핸들러
+    """
+    def __init__(self, client_id, client_secret, logger = None):
+
+        if logger is None:
+            self.logger = logging.getLogger("root")
+        else:
+            self.logger = logger
+
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+        self.keywordGroups = []
+        self.url = "https://openapi.naver.com/v1/datalab/search"
+
+    def add_keyword_groups(self, group_dict):
+        """
+        검색어 그룹 추가
+        """
+
+        keyword_gorup = {
+            'groupName': group_dict['groupName'],
+            'keywords': group_dict['keywords']
+        }
+        
+        self.keywordGroups.append(keyword_gorup)
+        self.logger.info(f"Num of keywordGroups: {len(self.keywordGroups)}")
+
+    def get_data(self, startDate, endDate, timeUnit, device, ages, gender):
+        """
+        요청 결과 반환
+        timeUnit - 'date', 'week', 'month'
+        device - None, 'pc', 'mo'
+        ages = [], ['1' ~ '11']
+        gender = None, 'm', 'f'
+        """
+
+        # Request body
+        body = json.dumps({
+            "startDate": startDate,
+            "endDate": endDate,
+            "timeUnit": timeUnit,
+            "keywordGroups": self.keywordGroups,
+            "device": device,
+            "ages": ages,
+            "gender": gender
+        }, ensure_ascii=False)
+        
+        # Results
+        request = urllib.request.Request(self.url)
+        request.add_header("X-Naver-Client-Id",self.client_id)
+        request.add_header("X-Naver-Client-Secret",self.client_secret)
+        request.add_header("Content-Type","application/json")
+        response = urllib.request.urlopen(request, data=body.encode("utf-8"))
+        rescode = response.getcode()
+        if(rescode==200):
+            # Json Result
+            result = json.loads(response.read())
+            
+            df = pd.DataFrame(result['results'][0]['data'])[['period']]
+            for i in range(len(self.keywordGroups)):
+                tmp = pd.DataFrame(result['results'][i]['data'])
+                tmp = tmp.rename(columns={'ratio': result['results'][i]['title']})
+                df = pd.merge(df, tmp, how='left', on=['period'])
+            self.df = df.rename(columns={'period': '날짜'})
+            self.df['날짜'] = pd.to_datetime(self.df['날짜'])
+            
+        else:
+            self.logger.info("Error Code:" + rescode)
+            
+        return self.df
